9a292abd57fd3aaf58e2e890cc6089f07e57379b
Mark Carey <markust007@hotmail.com>
2020-10-29 16:38:59 -0500

    hide db

--- backend/config/properties.js
+++ backend/config/properties.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT : process.env.PORT || 3001,
     // DB : 'mongodb://localhost/node-project'
-    DB : 'mongodb+srv://markust007:Leonidas007@cluster0-ibnpa.mongodb.net/test?retryWrites=true&w=majority'
+    DB : ''
 }
